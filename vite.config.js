035f307b0c433677def5c0be4b22ebaa8b1d89cf
Amritanshu Kumar <amritanshukumar2001@gmail.com>
2026-05-07 03:22:48 +0530

    backend updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5050",
+      "/api": "https://hyprtip.onrender.com",
     },
   },
 });
